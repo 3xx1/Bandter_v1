14ab92860d9c67de82fcf780326c7ef6d1f795cd
J C <joshcoppola@gmail.com>
2015-01-13 01:09:17 -0800

    Create basic file upload function

--- js/dev/uploadMusic.js
+++ js/dev/uploadMusic.js
@@ -1,5 +1,7 @@
 function uploadMusic() {
     var fileUploadControl = $("#musicFileUpload")[0];
+    
+    // TODO - handle multiple file uploading
     if (fileUploadControl.files.length > 0) {
         var file = fileUploadControl.files[0];
         var name = fileUploadControl.files[0].name;
@@ -7,11 +9,12 @@ function uploadMusic() {
         var parseFile = new Parse.File(name, file);
 
         parseFile.save().then(function() {
-            // The file has been saved to Parse.
+            // Must create a class with a reference to the uploaded file in order to access it
             var TestMusic = Parse.Object.extend("TestMusic");
             var testMusic = new TestMusic();
             
             testMusic.set('fileReference', parseFile);
+            testMusic.set('annotations', []);
             testMusic.save();
             
             console.log('Done')
