1e32cb9ea12c2bc5d31885a5310c19bdb8245bd5
Josh <joshuac@tyemill.com>
2015-01-13 11:40:29 -0800

    Add basic ability to change recording name and folder name before upload

--- js/dev/uploadMusic.js
+++ js/dev/uploadMusic.js
@@ -1,11 +1,23 @@
+function showUploadForm() {
+    // Folder name defaults to today's date
+    var d = new Date();
+    var folderName = d.getFullYear() + "/" + (d.getMonth() + 1) + "/" + d.getDate() + ' jam';
+    // Try to remove last 4 characters of filename - not working for some reason: 
+    //var recordingName = document.getElementById("musicFileUpload")[0].files[0].name;
+    //recordingName = recordingName.substring(0, recordingName.length - 4);
+    document.getElementById("nameUploadFile").className = "";
+    document.getElementById("recordingName").value = $("#musicFileUpload")[0].files[0].name;
+    //document.getElementById("recordingName").value = recordingName;
+    document.getElementById("recordingFolderName").value = folderName;
+}
+
 function uploadMusic() {
     var fileUploadControl = $("#musicFileUpload")[0];
-    
     // TODO - handle multiple file uploading
     if (fileUploadControl.files.length > 0) {
         var file = fileUploadControl.files[0];
         var name = fileUploadControl.files[0].name;
-        
+
         var parseFile = new Parse.File(name, file);
 
         parseFile.save().then(function() {
@@ -13,12 +25,14 @@ function uploadMusic() {
             var AudioObject = Parse.Object.extend("AudioObject");
             var AudioObject = new AudioObject();
             
+            var recordingName = document.getElementById("recordingName").value;
+
             AudioObject.set('file', parseFile);
-            AudioObject.set('recordingName', name);
+            AudioObject.set('recordingName', recordingName);
             AudioObject.set('annotations', []);
             AudioObject.save();
             
-            console.log('Done')
+            console.log('Done uploading ' + recordingName)
             
         }, function(error) {
             // The file either could not be read, or could not be saved to Parse.
