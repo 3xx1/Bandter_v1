8787dd42c9634e235ba3173ad57fd61d3ff8d2bf
Josh <joshuac@tyemill.com>
2015-01-18 13:59:54 -0800

    Create function to query folders (and recordings in those folders)

---
+++ js/dev/getFolders.js
@@ -0,0 +1,36 @@
+
+
+// Function is called on page load to get all the folders (and recordings within the folders) as sidebar nav items
+// There is probably a different approach that needs to be used so this entire function may need to be removed or rewritten
+function getFolders() {
+  var Folder = Parse.Object.extend("Folder");
+  var folderQuery = new Parse.Query(Folder);
+  // Below line indicates that we need to include the elements inside the "recordings" array in our query.
+  // This makes it so that we can later use .get() on the objects pointed to by this array, in order to
+  // display these recordings in the navbar
+  folderQuery.include("recordings");
+
+  folderQuery.find({
+    success: function(results) {
+      console.log("Successfully retrieved " + results.length + " folders.");
+      // Do something with the returned Parse.Object values
+      for (var i = 0; i < results.length; i++) {
+        var folder = results[i];
+        // Append an li to the sidebar, containing the name of the folder
+        $("#folderList").append('<li><a> ' + folder.get('name') + ' (folder) </a></li>');
+
+        // Loop through all files in the folder and display them
+        var recordings = folder.get('recordings');
+        for (var j = 0; j < recordings.length; j++) {
+          var recording = recordings[j];
+          // Temporary code to add the recording name as a link on the navbar. There is probably a much more elegant and useful way to do this.
+          $("#folderList").append('<li><a> -- ' + recording.get('recordingName') + '</a></li>');
+        }
+
+      }
+    },
+    error: function(error) {
+      console.log("Error: " + error.code + " " + error.message);
+    }
+  });
+}
